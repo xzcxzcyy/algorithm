40137cf1b40700ce4d180793a00a88498ed3ac51
Z.C.Xu <xzcxzcyy@126.com>
2017-06-13 19:39:57 +0800

    add a_plus_b.cpp

---
+++ [COGS_1]a_plus_b/a_plus_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+//#define DEBUG
+
+using namespace std;
+
+double a, b;
+
+int main()
+{
+#ifndef DEBUG
+    freopen("aplusb.in", "r", stdin);
+    freopen("aplusb.out", "w", stdout);
+#endif
+    cin >> a >> b;
+    cout << setiosflags(ios::fixed) << setprecision(0) << a + b << endl;
+    return 0;
+}
