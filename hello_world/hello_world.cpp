cc721cc14f3947c73288005d7587f59c20aab6ef
Z.C.Xu <xzcxzcyy@126.com>
2019-07-31 21:34:29 +0800

    Revert "7.31 01backpack"
    This reverts commit 9981d7bbd06a4641b258cd5d6e3083a871a18c7e.

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -4,13 +4,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    cout << "helloworld" << endl;
-=======
-    //int a=0;
-    cout<<"hello world"<<endl;
-    cin.get();
-    
->>>>>>> 4f266a07e0167d56ccb8b806b876e91862e83bdc
+    cout<<"helloworld"<<endl;
     return 0;
 }
