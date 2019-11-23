bb6a71843ecaf41a7c85f52cda9bb67e2991dcb2
Banson Xu <xzcxzcyy@126.com>
2019-11-23 11:11:33 +0800

    debugging: issues occurred when big data is provided to mqsort

--- qsort/qsort.c
+++ qsort/qsort.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <inttypes.h>
+#include <assert.h>
+#pragma GCC("-W1,--stack=128000000")
 
 void mqsort(int *begin, int *end);
 void swap(int *x, int *y);
 int mrand(int length);
 
-int arr[100000];
+uint32_t xor128(void)
+{
+    static uint32_t x = 123456789;
+    static uint32_t y = 362436069;
+    static uint32_t z = 521288629;
+    static uint32_t w = 88675123;
+    uint32_t t;
 
+    t = x ^ (x << 11);
+    x = y;
+    y = z;
+    z = w;
+    return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
+
+int arr[100010];
 
 int main()
 {
+    //freopen(".\\qsort\\testdata.in", "r", stdin);
     //srand(time(NULL));
     int n;
     scanf("%d", &n);
@@ -19,6 +37,7 @@ int main()
         scanf("%d", &arr[i]);
     }
     mqsort(arr, arr + n);
+    printf("%d", n);
     for (int i = 0; i < n; ++i)
     {
         printf("%d ", arr[i]);
@@ -35,7 +54,7 @@ void mqsort(int *begin, int *end)
     swap(begin, begin + (end - begin) / 2);
     int *p = begin;
     int *a = begin, *b = end - 1;
-    while (a != b)
+    while (a < b)
     {
         if (*p <= *b)
         {
@@ -64,5 +83,20 @@ void swap(int *x, int *y)
 
 int mrand(int length)
 {
-    return rand() % length;
+    int a = rand() % length;
+    int b = rand() % length;
+    int c = rand() % length;
+
+    if (a <= b && b <= c || c <= b && b <= a)
+    {
+        return b;
+    }
+    else if (b <= a && a <= c || c <= a && a <= b)
+    {
+        return a;
+    }
+    else
+    {
+        return c;
+    }
 }
